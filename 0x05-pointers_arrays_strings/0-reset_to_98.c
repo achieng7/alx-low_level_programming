9aca12fa7e5b1f929e8dd091cd45fda2c9e9b227
achieng7 <achiengbryan710@gmail.com>
2023-02-27 08:23:43 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * takes a pointer and updates the value it point to
+ * reset_to_98 - takes a pointer and updates the value it point to
  * @n: pointer
  * Return: void
  */
