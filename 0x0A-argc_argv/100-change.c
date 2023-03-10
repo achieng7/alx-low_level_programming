a5b08b94ecb7e3749e80b103cca76ebe43dc61de
achieng7 <achiengbryan710@gmail.com>
2023-03-10 12:44:48 -0800

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
+
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
+* main - prints the minimum number of coins for an amount of money
+* @argc: should count two arguments
+* @argv: arguments given should be program name and amount of money
+* Return: least number of coins, 0 if negative amount, 1 if amount not given
+*/
+
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+int n, coins = 0;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[i]);
-	}
-	printf("%d\n", add);
-	return (0);
+/* validate input */
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+if (argv[1][0] == '-')
+{
+printf("0\n");
+return (0);
+}
+
+/* convert string to int and calculate coins */
+n = atoi(argv[1]);
+
+coins += n / 25;
+n = n % 25;
+coins += n / 10;
+n = n % 10;
+coins += n / 5;
+n = n % 5;
+coins += n / 2;
+n = n % 2;
+coins += n / 1;
+
+printf("%d\n", coins);
+return (0);
 }
